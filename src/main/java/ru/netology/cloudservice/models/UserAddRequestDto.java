67c377712e6c7fb17ebd96d976b1e6c6d1637662
APKonovalov <APKonovalov@alfabank.ru>
2024-02-03 15:25:51 +0300

    fix #3 remove user add functionality

--- src/main/java/ru/netology/cloudservice/models/UserAddRequestDto.java
+++
@@ -1,8 +0,0 @@
-package ru.netology.cloudservice.models;
-
-import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBlank;
-
-public record UserAddRequestDto(@NotBlank(message = "login can not be null") String login,
-                                @NotBlank(message = "login can not be null") String password) {
-
-}
--- src/main/java/ru/netology/cloudservice/services/UsersService.java
+++
@@ -1,8 +0,0 @@
-package ru.netology.cloudservice.services;
-
-import ru.netology.cloudservice.models.UserAddRequestDto;
-
-public interface UsersService {
-
-    void addUser(UserAddRequestDto requestDto);
-}
